48c330722d17609668096295afd0d44c6027f567
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2023-01-01 21:12:41 +0530

    Adding expenses with props using loop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      locationOfExpenditure: "in a mall",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      locationOfExpenditure: "in a car petrol",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      locationOfExpenditure: "in a theater",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      locationOfExpenditure: "in a shopping",
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible</p>
-      <ExpenseItem/>
-      <ExpenseItem/>
-      <ExpenseItem/>
+      {expenses.map((expense) => {
+        const item = (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            locationOfExpenditure={expense.locationOfExpenditure}
+            date={expense.date}
+          ></ExpenseItem>
+        );
+        return item;
+      })}
 
     </div>
   );
