1abb20ea0ae61bc06e544d965598abff664a8f52
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2023-01-01 15:00:49 +0530

    Adding expense item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+  <div>
+    <div>March 28th 2022</div>
+    <div>
+      <h2>Car Insurance</h2>
+      <div>Rs133</div>
+    </div>
+  </div>
+  )
+}
+
+export default ExpenseItem;
