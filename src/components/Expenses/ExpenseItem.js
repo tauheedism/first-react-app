ba9449df4a2736ac2b01b38fb0511d7a5d870797
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2023-01-02 12:22:44 +0530

    Delete Button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,12 +4,25 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  let title = props.title
+  // let id = props.id;
+
+  const deleteHandler = () => 
+  {
+    console.log('delete')
+  }
+  const eventHandler = () => {
+      title = "updated";
+      console.log('Updated!');
+  }
   return (
     <Card className="expense-item">
       <div>
         <ExpenseDate date={props.date} />
       </div>
-      <ExpenseDetails amount={props.amount} title={props.title} />
+      <ExpenseDetails amount={props.amount} title={title} />
+      <button onClick={eventHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete Expense</button>
     </Card>
   );
 }
